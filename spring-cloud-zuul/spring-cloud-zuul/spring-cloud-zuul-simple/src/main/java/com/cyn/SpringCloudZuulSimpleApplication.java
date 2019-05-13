a664dd6798db5d8419462bcce8684ba03a5218b7
cyn <599283258@qq.com>
2019-05-13 17:35:21 +0800

    spring-cloud-zuul

---
+++ spring-cloud-zuul/spring-cloud-zuul/spring-cloud-zuul-simple/src/main/java/com/cyn/SpringCloudZuulSimpleApplication.java
@@ -0,0 +1,13 @@
+package com.cyn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringCloudZuulSimpleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringCloudZuulSimpleApplication.class, args);
+	}
+
+}
