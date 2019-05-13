f5a3b03635ced4de59eb077678ec8e06f40bb8cc
cyn <599283258@qq.com>
2019-05-13 09:55:15 +0800

    springcloud git配置中心文件修改

---
+++ spring-cloud-config-git/spring-cloud-config-client/src/main/java/com/cyn/web/HelloController.java
@@ -0,0 +1,17 @@
+package com.cyn.web;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+class HelloController {
+
+    @Value("${cyn.hello}")
+    private String hello;
+
+    @RequestMapping("/hello")
+    public String from() {
+        return this.hello;
+    }
+}
